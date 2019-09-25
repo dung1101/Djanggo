f2cd13e8e70293e23dadd9c50242fa48d020720a
dung1101 <mrbeo1221119@gmail.com>
2019-09-25 22:46:07 +0700

    update

--- projects/ModelCustom/app/admin.py
+++ projects/ModelCustom/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import HocSinh, Lop
 
 # Register your models here.
+@admin.register(HocSinh)
+class HocSinhAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Lop)
+class LopAdmin(admin.ModelAdmin):
+    pass
